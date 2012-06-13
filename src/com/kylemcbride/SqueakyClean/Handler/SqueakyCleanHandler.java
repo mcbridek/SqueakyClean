fccc1a43a3a86160b5a6cb2ce078e1bf603bc2d0
Kyle McBride <info@kylemcbride.com>
2012-06-13 12:04:48 +0100

    Creating the Splash Page

--- src/com/kylemcbride/SqueakyClean/Handler/SqueakyCleanHandler.java
+++ src/com/kylemcbride/SqueakyClean/Handler/SqueakyCleanHandler.java
@@ -1,5 +1,38 @@
 package com.kylemcbride.SqueakyClean.Handler;
 
-public class SqueakyCleanHandler {
+import com.kylemcbride.SqueakyClean.R;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class SqueakyCleanHandler implements OnClickListener {
+	
+	protected String TAG = "Activity Handler";
+	
+	@Override
+	public void onClick(View v) {
+		System.out.println("Button being clicked");
+		
+		Log.d(TAG, "Listening");
+		switch (v.getId()) {
+		
+		case R.id.currentCustomer:
+			Toast.makeText(v.getContext(), "You pressed " + ((Button)v).getText(), Toast.LENGTH_LONG).show();
+			break;
+			
+		case R.id.newCustomer:
+			Toast.makeText(v.getContext(), "You pressed " + ((Button)v).getText(), Toast.LENGTH_LONG).show();
+			break;
+		
+		default:
+			Log.d(TAG, "Launching a new page.");
+			Toast.makeText(v.getContext(), "You pressed " + ((Button)v).getText(), Toast.LENGTH_LONG).show();
+			break;
+		}
+		
+
+	}
 
 }
--- src/com/kylemcbride/SqueakyClean/SqueakyCleanActivity.java
+++ src/com/kylemcbride/SqueakyClean/SqueakyCleanActivity.java
@@ -1,15 +1,30 @@
 package com.kylemcbride.SqueakyClean;
 
+import com.kylemcbride.SqueakyClean.Handler.SqueakyCleanHandler;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class SqueakyCleanActivity extends Activity {
-    /** Called when the activity is first created. */
+    
+    protected String TAG = "SqueakyClean";
+	
+	/** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) 
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        //getActionBar().hide(); 
+        SqueakyCleanHandler activityHandler = new SqueakyCleanHandler();
+        
+        
+        Button newCustomer =  (Button) findViewById(R.id.newCustomer);
+        newCustomer.setOnClickListener((OnClickListener) activityHandler);
+        
+        Button currentCustomer = (Button) findViewById(R.id.currentCustomer);
+        currentCustomer.setOnClickListener((OnClickListener) activityHandler);
     }
+    
 }
